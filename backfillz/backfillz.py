6681291353d025f8fb1e4eaca8e5cc63ff5752f2
Roly Perera <roly.perera@dynamicaspects.org>
2021-03-29 18:00:23 +0100

    Start fleshing out as_backfillz.

--- backfillz/backfillz.py
+++ backfillz/backfillz.py
@@ -1,4 +1,5 @@
 from typing import Any
+from stan.fit import Fit
 
 
 class Backfillz:
@@ -6,5 +7,31 @@ class Backfillz:
         pass
 
 
-def as_backfillz(obj: Any, verbose: bool) -> Backfillz:
-    return Backfillz()
+def as_backfillz(fit: Fit, verbose: bool) -> Backfillz:
+    # create backfillz object
+    backfillz_object = Backfillz()
+
+    # populate backfillz object and set theme
+#  if (class(object) == "stanfit") {
+#    backfillz_object@mcmc_samples <-
+#      rstan::extract(object, permuted = FALSE, inc_warmup = TRUE)
+#    backfillz_object@mcmc_model <- object@stanmodel@model_code
+#  } else if (class(object) == "data.frame") {
+#    backfillz_object@mcmc_samples <- df_to_stanarray(object)
+#    backfillz_object@mcmc_model <- "Samples imported from dataframe"
+#  }
+
+  # set default theme
+#  backfillz_object <- set_theme(backfillz_object, verbose)
+
+  # initialise plot history
+#  backfillz_object@plot_history <- data.frame(
+#    ID = 1,
+#    Date = date(),
+#    Event = "Object Creation",
+#    R_version = R.Version()$version.string,
+#    Saved = FALSE,
+#    stringsAsFactors = FALSE
+#  )
+
+    return backfillz_object
--- tests/test_backfillz.py
+++ tests/test_backfillz.py
@@ -1,8 +1,14 @@
 """Test module for backfillz."""
 
-from backfillz import as_backfillz
+from backfillz.backfillz import as_backfillz
+from tests.generate_sample_fit import generate_fit
 
 
 def test() -> None:
     # "that backfillz object can be created"
-    sample_backfillz = as_backfillz(sample_stanfit, verbose = false)
+    sample_backfillz = as_backfillz(generate_fit(), verbose=False)
+    print(sample_backfillz)
+
+
+if __name__ == '__main__':
+    test()
