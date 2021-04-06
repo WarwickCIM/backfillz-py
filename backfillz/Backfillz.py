5aa4335be5cd9e62b1698513f1bad959cfc06084
Roly Perera <roly.perera@dynamicaspects.org>
2021-04-06 08:30:56 +0100

    HistoryEntry, HistoryEvent.

--- backfillz/Backfillz.py
+++ backfillz/Backfillz.py
@@ -1,27 +1,63 @@
-import pandas as pd  # type: ignore
+from datetime import datetime
+from enum import Enum
+import sys
+from typing import List
+
 from stan.fit import Fit  # type: ignore
 
 from backfillz.BackfillzTheme import BackfillzTheme, default, demo_1, demo_2, solarized_dark
 
 
+class HistoryEvent(Enum):
+    """The kind of history event."""
+
+    OBJECT_CREATION = 1
+
+
+class HistoryEntry:
+    """An entry in the Backfillz history log."""
+
+    def __init__(
+        self,
+        ident: int,
+        date: datetime,
+        event: HistoryEvent,
+        python_version: str,  # more specific type?
+        saved: bool,
+        strings_as_factors: bool  # not sure what this is
+    ) -> None:
+        """Create a history entry."""
+        self.ident = ident,
+        self.date = date,
+        self.event = event,
+        self.python_version = python_version,
+        self.saved = saved,
+        self.strings_as_factors = strings_as_factors
+
+
 class Backfillz:
     """Represents a Backfillz user session."""
 
-    fit: Fit
     theme: BackfillzTheme
-    plot_history: pd.DataFrame
+    plot_history: List[HistoryEntry]
 
-    def __init__(self, mcmc_samples: Fit) -> None:
+    def __init__(
+        self,
+        fit: Fit
+    ) -> None:
         """Initialise a Backfillz session."""
-        #  backfillz_object@plot_history <- data.frame(
-        #    ID = 1,
-        #    Date = date(),
-        #    Event = "Object Creation",
-        #    R_version = R.Version()$version.string,
-        #    Saved = FALSE,
-        #    stringsAsFactors = FALSE
-        #  )
-        pass
+        fit = fit
+        self.set_theme("default", False)
+        self.plot_history = [
+            HistoryEntry(
+                ident=1,
+                date=datetime.now(),
+                event=HistoryEvent.OBJECT_CREATION,
+                python_version=sys.version,
+                saved=False,
+                strings_as_factors=False
+            )
+        ]
 
     def set_theme(self, theme: str, verbose: bool = True) -> None:
         """Set Backfillz theme."""
@@ -41,6 +77,4 @@ class Backfillz:
 
 def as_backfillz(fit: Fit, verbose: bool) -> Backfillz:
     """Create a Backfillz session from a PyStan fit."""
-    backfillz = Backfillz(fit)
-    backfillz.set_theme("default", verbose)
-    return backfillz
+    return Backfillz(fit)
