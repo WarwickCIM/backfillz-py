58cd278dd6ad20305edaf23b7c8c3c4f810990e8
Roly Perera <roly.perera@dynamicaspects.org>
2021-06-01 17:09:28 +0100

    Start fleshing out trace_dial.

--- backfillz/trace_dial.py
+++ backfillz/trace_dial.py
@@ -1,4 +1,25 @@
-class TraceDial:
+from backfillz.plot import LeafPlot, RootPlot, VerticalSubplots
+
+
+class DialPlot(LeafPlot):
+    pass
+
+
+class Histograms(VerticalSubplots):
+    pass
+
+
+class TraceDial(RootPlot):
     """Top-level plot, for a given parameter and chain."""
 
-    pass
+    @property
+    def plots(self):
+        return [self.dial_plot, self.histograms]
+
+    @property
+    def dial_plot(self) -> DialPlot:
+        return DialPlot()
+
+    @property
+    def histograms(self) -> Histograms:
+        return Histograms()
