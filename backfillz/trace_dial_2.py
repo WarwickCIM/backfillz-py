107fdaebc7723a75309d33bb2e40ed2145111caf
Roly Perera <roly.perera@dynamicaspects.org>
2021-06-07 16:14:56 +0100

    Skeleton of new experiment.

--- backfillz/trace_dial_2.py
+++ backfillz/trace_dial_2.py
@@ -1,14 +1,27 @@
 from dataclasses import dataclass
+from typing import List
 
+import numpy as np
 import plotly.graph_objects as go
 
-from backfillz.plot import RootPlot
+from backfillz.core import Backfillz, ParameterSlices, Slice
+from backfillz.plot import Plot, RootPlot
 
 
 # Experiment to see if I can add axes directly, without involving make_subplots.
 @dataclass
-class TraceDial(RootPlot):
+class TraceDial2(RootPlot):
     """Top-level trace dial plot for a given parameter."""
+
+    data: ParameterSlices
+
+    @property
+    def title(self) -> str:
+        return f"Pretzel plot for {self.data.param}"
+
+    @property
+    def plots(self) -> List[Plot]:
+        return []
 
     # Override render to customise layout.
     def render(self) -> None:
@@ -19,6 +32,22 @@ class TraceDial(RootPlot):
                 titlefont=dict(size=30),
                 plot_bgcolor=self.theme.bg_colour,
                 showlegend=False,
+                xaxis=dict(
+                    domain=[0, 1],
+                    anchor='y'
+                ),
+                yaxis=dict(
+                    domain=[0, 1],
+                    anchor='x'
+                ),
+                xaxis2=dict(
+                    domain=[0.5, 1],
+                    anchor='y2',
+                ),
+                yaxis2=dict(
+                    domain=[0.5, 1],
+                    anchor='x2'
+                )
             )
         )
 
@@ -31,3 +60,17 @@ class TraceDial(RootPlot):
             plot.render(fig)
 
         fig.show(config=dict(displayModeBar=False, showAxisDragHandles=False))
+
+    @staticmethod
+    def plot(backfillz: Backfillz, save_plot: bool = False) -> None:
+        slcs: List[Slice] = [Slice(0.0, 0.04), Slice(0.4, 1)]  # how to decide
+        param: str = backfillz.params[0]  # pick first parameter for now (mu)
+        data: ParameterSlices = ParameterSlices(
+            slcs=slcs,
+            param=param,
+            chains=backfillz.iter_chains(param),
+            max_sample=np.amax(backfillz.mcmc_samples[param]),
+            min_sample=np.amin(backfillz.mcmc_samples[param]),
+        )
+
+        TraceDial2(backfillz.theme, data).render()
--- tests/test_backfillz.py
+++ tests/test_backfillz.py
@@ -6,6 +6,7 @@ from tests.generate_sample_fit import generate_fit, Stan
 from backfillz.core import Backfillz
 from backfillz.theme import demo_1
 from backfillz.trace_dial import TraceDial
+from backfillz.trace_dial_2 import TraceDial2
 from backfillz.trace_slice_histogram import TraceSliceHistogram
 
 
@@ -26,7 +27,7 @@ def test_sample_fit(stan: Stan) -> None:
     assert expected_stan.equal(stan)
 
 
-# @pytest.mark.skip(reason="temporarily disable")
+@pytest.mark.skip(reason="temporarily disable")
 def test_trace_slice_histogram(stan: Stan) -> None:
     """Slice histogram plot is generated without error."""
     backfillz = Backfillz(stan.fit)
@@ -34,8 +35,16 @@ def test_trace_slice_histogram(stan: Stan) -> None:
     TraceSliceHistogram.plot(backfillz)
 
 
+@pytest.mark.skip(reason="temporarily disable")
 def test_trace_dial(stan: Stan) -> None:
     """Trace dial plot is generated without error."""
     backfillz = Backfillz(stan.fit)
     backfillz.set_theme(demo_1, False)
     TraceDial.plot(backfillz)
+
+
+def test_trace_dial_2(stan: Stan) -> None:
+    """Trace dial plot is generated without error."""
+    backfillz = Backfillz(stan.fit)
+    backfillz.set_theme(demo_1, False)
+    TraceDial2.plot(backfillz)
