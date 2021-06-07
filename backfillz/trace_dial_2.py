dfcf036480f2774e65a03bb08c739aacd0ed3784
Roly Perera <roly.perera@dynamicaspects.org>
2021-06-07 18:03:16 +0100

    Validate idea of creating axes and then assigning traces to axes.

--- backfillz/trace_dial_2.py
+++ backfillz/trace_dial_2.py
@@ -1,11 +1,47 @@
 from dataclasses import dataclass
+from math import ceil, floor
 from typing import List
 
 import numpy as np
+from plotly.basedatatypes import BaseTraceType  # type: ignore
 import plotly.graph_objects as go
 
-from backfillz.core import Backfillz, ParameterSlices, Slice
-from backfillz.plot import Plot, RootPlot
+from backfillz.core import Backfillz, ParameterSlices, Props, Slice
+from backfillz.plot import Plot, RootPlot, segment, VerticalSubplots
+from backfillz.slice_histograms import SliceHistogram
+
+
+@dataclass
+class TraceDialHistogram(SliceHistogram):
+    """Slice histogram for trace dial plot."""
+
+    @property
+    def xaxis_props(self) -> Props:
+        if self.n_slc == len(self.data.slcs) - 1:
+            return dict(side='top')
+        else:
+            return dict(visible=False)
+
+
+@dataclass
+class SliceHistograms(VerticalSubplots):
+    """Two slice histograms, one for burn in, one for rest of chain."""
+
+    def make_plots(self) -> List[Plot]:
+        return [
+            TraceDialHistogram(
+                axis_id=self.axis_ids[n],
+                x_domain=self.x_domain,
+                y_domain=segment(self.y_domain, len(self.data.slcs), n),
+                data=self.data,
+                theme=self.theme,
+                slc=slc,
+                n_slc=n,
+                row=self.row + len(self.data.slcs) - 1 - n,
+                col=self.col,
+            )
+            for n, slc in enumerate(self.data.slcs)
+        ]
 
 
 # Experiment to see if I can add axes directly, without involving make_subplots.
@@ -20,8 +56,36 @@ class TraceDial2(RootPlot):
         return f"Pretzel plot for {self.data.param}"
 
     @property
-    def plots(self) -> List[Plot]:
-        return []
+    def plot_elements(self) -> List[BaseTraceType]:
+        return self.traces + [self.histogram]
+
+    # one per chain
+    @property
+    def traces(self) -> List[go.Scatter]:
+        return [
+            go.Scatter(
+                x=chain,
+                y=list(range(0, self.data.n_iter)),
+                line=dict(color=self.theme.palette[n]),
+                xaxis='x',
+                yaxis='y',
+            )
+            for n, chain in enumerate(self.data.chains)
+        ]
+
+    @property
+    def histogram(self) -> go.Histogram:
+        return go.Histogram(
+            x=[x for xs in self.data.chain_slices(self.data.slcs[0]) for x in xs],
+            xbins=dict(start=floor(self.data.min_sample), end=ceil(self.data.max_sample), size=1),
+            marker=dict(
+                color=self.theme.bg_colour,
+                line=dict(color=self.theme.fg_colour, width=1)
+            ),
+            histnorm='probability',
+            xaxis='x2',
+            yaxis='y2',
+        )
 
     # Override render to customise layout.
     def render(self) -> None:
@@ -34,30 +98,34 @@ class TraceDial2(RootPlot):
                 showlegend=False,
                 xaxis=dict(
                     domain=[0, 1],
-                    anchor='y'
+                    anchor='y',
                 ),
                 yaxis=dict(
                     domain=[0, 1],
-                    anchor='x'
+                    anchor='x',
                 ),
                 xaxis2=dict(
                     domain=[0.5, 1],
                     anchor='y2',
                 ),
                 yaxis2=dict(
+                    domain=[0.75, 1],
+                    anchor='x2',
+                ),
+                xaxis3=dict(
                     domain=[0.5, 1],
-                    anchor='x2'
+                    anchor='y3',
+                ),
+                yaxis3=dict(
+                    domain=[0.5, 0.75],
+                    anchor='x3',
                 )
             )
         )
+        print(fig.layout)
 
-        for plot in self.plots:
-            plot.layout_axes(fig)
-
-        self.add_additional_titles(fig)
-
-        for plot in self.plots:
-            plot.render(fig)
+        for el in self.plot_elements:
+            fig.add_trace(el)
 
         fig.show(config=dict(displayModeBar=False, showAxisDragHandles=False))
 
