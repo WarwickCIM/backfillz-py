c1425ccb59cc8691c6416a5ae934e43f6c71ad24
Roly Perera <roly.perera@dynamicaspects.org>
2021-06-08 11:39:09 +0100

    Fix lint errors.

--- tests/test_backfillz.py
+++ tests/test_backfillz.py
@@ -6,7 +6,6 @@ from tests.generate_sample_fit import generate_fit, Stan
 from backfillz.core import Backfillz
 from backfillz.theme import demo_1
 from backfillz.trace_dial import TraceDial
-from backfillz.trace_dial_2 import TraceDial2
 from backfillz.trace_slice_histogram import TraceSliceHistogram
 
 
