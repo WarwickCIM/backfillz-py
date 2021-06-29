4550b9f82d559205b137c2fdcb37ce6933a2866e
Roly Perera <roly.perera@dynamicaspects.org>
2021-06-29 17:20:15 +0100

    Fix lint errors.

--- backfillz/stan.py
+++ backfillz/stan.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 import pickle
+from typing import cast
 
 from stan.fit import Fit  # type: ignore
 from stan.model import Model  # type: ignore
@@ -23,7 +24,7 @@ class Stan:
     def load(file: str) -> Stan:
         """Load Stan model and fit using supplied file name."""
         with open(file + ".pkl", "rb") as f:
-            return pickle.load(f)
+            return cast(Stan, pickle.load(f))
 
     def equal(self, other: Stan) -> bool:
         """For now just compare fit for (str) equality. Model name seems to vary unpredictably."""
