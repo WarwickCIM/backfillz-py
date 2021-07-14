857b839313051c5484a7bdcc9515c1e911f03107
Roly Perera <roly.perera@dynamicaspects.org>
2021-07-14 09:00:58 +0100

    Extract donut_domain as parameter to polar_plot.

--- backfillz/trace_dial.py
+++ backfillz/trace_dial.py
@@ -45,16 +45,16 @@ class DialPlot(LeafPlot):
         return DialPlot.hole_size + y * (1 - DialPlot.hole_size)
 
     @staticmethod
-    def polar_plot(xs: List[float], ys: List[float]) -> Tuple[List[float], List[float]]:
+    def polar_plot(xs: List[float], ys: List[float], x_domain: Domain) -> Tuple[List[float], List[float]]:
         assert len(xs) == len(ys)
-        xs_ang = [DialPlot.to_angular(x, DialPlot.donut_domain) for x in normalise(xs)]
+        xs_ang = [DialPlot.to_angular(x, x_domain) for x in normalise(xs)]
         ys_radial = [DialPlot.to_radial(y) for y in normalise(ys)]
         return ([math.cos(x) * ys_radial[n] for n, x in enumerate(xs_ang)],
                 [math.sin(x) * ys_radial[n] for n, x in enumerate(xs_ang)])
 
     def polar_trace(self, n: int) -> go.Scatter:
         chain: np.ndarray = self.data.chains[n]
-        xs, ys = DialPlot.polar_plot([*range(0, len(chain))], [*chain])
+        xs, ys = DialPlot.polar_plot([*range(0, len(chain))], [*chain], DialPlot.donut_domain)
         return go.Scatter(x=xs, y=ys, line=dict(color=self.theme.palette[n]))
 
     @property
@@ -62,7 +62,7 @@ class DialPlot(LeafPlot):
         n_segments: int = 100
         xs = [0] + [*range(0, n_segments)] + [n_segments - 1] + [*range(n_segments - 1, -1, -1)]
         ys = [0.0] + [1.0] * n_segments + [1.0] + [0.0] * n_segments
-        x, y = DialPlot.polar_plot(xs, ys)
+        x, y = DialPlot.polar_plot(xs, ys, DialPlot.donut_domain)
         return go.Scatter(x=x, y=y, line=dict(width=0), fill='toself', fillcolor=self.theme.mg_colour)
 
     @property
