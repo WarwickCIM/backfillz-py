e72c4f48e047ef85e7e66282bed49f3c27104c1b
Roly Perera <roly.perera@dynamicaspects.org>
2021-07-15 14:09:52 +0100

    Make shared y_axis for all polar traces and pass to polar_trace.

--- backfillz/trace_dial.py
+++ backfillz/trace_dial.py
@@ -74,7 +74,7 @@ class DialPlot(LeafPlot):
             DialPlot.donut_segment(DialPlot.slice_domain(remaining), colours.remaining_segment)
         ]
 
-    def polar_trace(self, n: int, x_axis) -> go.Scatter:
+    def polar_trace(self, n: int, x_axis: Axis, y_axis: Axis) -> go.Scatter:
         chain: np.ndarray = self.data.chains[n]
         xs, ys = DialPlot.polar_plot([*range(0, len(chain))], [*chain], x_axis)
         return go.Scatter(x=xs, y=ys, line=dict(color=self.theme.palette[n]))
@@ -82,7 +82,8 @@ class DialPlot(LeafPlot):
     @property
     def polar_traces(self) -> List[go.Scatter]:
         x_axis = Axis((0, self.data.n_iter), DialPlot.angular_domain)
-        return [self.polar_trace(n, x_axis) for n, _ in enumerate(self.data.chains)]
+        y_axis = Axis((self.data.min_sample, self.data.max_sample), DialPlot.radial_domain)
+        return [self.polar_trace(n, x_axis, y_axis) for n, _ in enumerate(self.data.chains)]
 
 
 @dataclass
