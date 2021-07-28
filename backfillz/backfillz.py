658116b906657d56f62c682908435c966b293406
Roly Perera <roly.perera@dynamicaspects.org>
2021-07-28 08:09:48 +0100

    Remaining expected outputs.

--- backfillz/backfillz.py
+++ backfillz/backfillz.py
@@ -72,12 +72,14 @@ class Backfillz:
         fig = TraceSliceHistogram.fig(self.mcmc_run, self.theme, self.verbose, param)
         self.plot_history.append(HistoryEntry(HistoryEvent.SLICE_HISTOGRAM, save_plot))
         fig.show(config=default_config())
+        expect_fig(fig, "tests/expected_slice_histogram")
 
     def plot_trace_dial(self, param: str, save_plot: bool = False) -> None:
         """Create and plot a trace dial."""
         fig = TraceDial.fig(self.mcmc_run, self.theme, self.verbose, param)
         self.plot_history.append(HistoryEntry(HistoryEvent.TRACE_DIAL, save_plot))
         fig.show(config=default_config())
+        expect_fig(fig, "tests/expected_trace_dial")
 
     def plot_spiral_stream(self, param: str, save_plot: bool = False) -> None:
         """Create and plot a spiral stream."""
