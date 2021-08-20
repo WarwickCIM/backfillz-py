77dcf1efb38065e62dbaff1f0d6a6a09abf8d386
Roly Perera <roly.perera@dynamicaspects.org>
2021-08-20 18:21:37 +0100

    INLINE/RENAME VARS in donut_segment.

--- backfillz/trace_dial.py
+++ backfillz/trace_dial.py
@@ -64,12 +64,11 @@ class DialPlot(LeafPlot):
         n_steps: int = math.floor(100 * size(x_domain) / size(DialPlot.angular_domain))
         x_axis = Axis((0, n_steps - 1), x_domain)
         y_axis = Axis((0, 1), DialPlot.radial_domain)
-        xs = [*range(n_steps - 1, -1, -1)]
-        ys = [0.0] * n_steps
+
         xs0, ys0 = polar_plot([*range(0, n_steps)], [1.0] * n_steps, x_axis, y_axis)
-        xs, ys = polar_plot(xs, ys, x_axis, y_axis)
+        xs1, ys1 = polar_plot([*range(n_steps - 1, -1, -1)], [0.0] * n_steps, x_axis, y_axis)
         return go.Scatter(
-            x=xs0 + xs, y=ys0 + ys,
+            x=xs0 + xs1, y=ys0 + ys1,
             line=dict(width=0),
             mode='lines',
             fill='toself',
